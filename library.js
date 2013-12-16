ae69b720659497b85481a44b749e16ae8f500406
psychobunny <psycho.bunny@hotmail.com>
2013-12-16 15:22:13 -0500

    updated plugin to show an example of how to use the template system

--- library.js
+++ library.js
@@ -4,8 +4,39 @@ var	fs = require('fs'),
 	
 var constants = Object.freeze({
 	'route': '/about',
-	'name': "About NodeBB"
+	'name': "About NodeBB",
+	'gallery': [
+		{
+			image: "http://designcreateplay.com/portfolio/work/nodebb1.png",
+			thumb: "http://designcreateplay.com/portfolio/work/nodebb1_thumb.png"
+		},
+		{
+			image: "http://designcreateplay.com/portfolio/work/nodebb2.png",
+			thumb: "http://designcreateplay.com/portfolio/work/nodebb2_thumb.png"
+		},
+		{
+			image: "http://designcreateplay.com/portfolio/work/nodebb3.png",
+			thumb: "http://designcreateplay.com/portfolio/work/nodebb3_thumb.png"
+		},
+		{
+			image: "http://designcreateplay.com/portfolio/work/nodebb4.png",
+			thumb: "http://designcreateplay.com/portfolio/work/nodebb4_thumb.png"
+		},
+		{
+			image: "http://designcreateplay.com/portfolio/work/nodebb5.png",
+			thumb: "http://designcreateplay.com/portfolio/work/nodebb5_thumb.png"
+		},
+		{
+			image: "http://designcreateplay.com/portfolio/work/nodebb6.png",
+			thumb: "http://designcreateplay.com/portfolio/work/nodebb6_thumb.png"
+		},
+		{
+			image: "http://designcreateplay.com/portfolio/work/nodebb7.png",
+			thumb: "http://designcreateplay.com/portfolio/work/nodebb_thumb7.png"
+		}
+	]
 });
+
 
 var StaticPage = {
 		addNavigation: function(custom_header, callback) {
@@ -19,6 +50,10 @@ var StaticPage = {
 		},
 		addRoute: function(custom_routes, callback) {
 			fs.readFile(path.resolve(__dirname, './static/about.tpl'), function (err, template) {
+				var content = templates.prepare(template.toString()).parse({
+					gallery: constants.gallery
+				});
+
 				custom_routes.routes.push({
 					"route": constants.route,
 					"method": "get",
@@ -28,7 +63,7 @@ var StaticPage = {
 							res: res,
 							route: constants.route,
 							name: constants.name,
-							content: template
+							content: content
 						});
 					}
 				});
--- static/about.tpl
+++ static/about.tpl
@@ -3,27 +3,11 @@
 	<p>This is a sample static page built into NodeBB</p>
 	<p>
 		<span class="thumbnails" data-toggle="lightbox" style="display: inline-block;">
-			<a href="http://designcreateplay.com/portfolio/work/nodebb1.png" data-ajaxify="false" class="thumbnail img-thumbnail">
-				<img src="http://designcreateplay.com/portfolio/work/nodebb1_thumb.png" alt="">
+			<!-- BEGIN gallery -->
+			<a href="{gallery.image}" data-ajaxify="false" class="thumbnail img-thumbnail">
+				<img src="{gallery.thumb}" alt="">
 			</a>
-			<a href="http://designcreateplay.com/portfolio/work/nodebb2.png" data-ajaxify="false" class="thumbnail img-thumbnail">
-				<img src="http://designcreateplay.com/portfolio/work/nodebb2_thumb.png" alt="">
-			</a>
-			<a href="http://designcreateplay.com/portfolio/work/nodebb3.png" data-ajaxify="false" class="thumbnail img-thumbnail">
-				<img src="http://designcreateplay.com/portfolio/work/nodebb3_thumb.png" alt="">
-			</a>
-			<a href="http://designcreateplay.com/portfolio/work/nodebb4.png" data-ajaxify="false" class="thumbnail img-thumbnail">
-				<img src="http://designcreateplay.com/portfolio/work/nodebb4_thumb.png" alt="">
-			</a>
-			<a href="http://designcreateplay.com/portfolio/work/nodebb5.png" data-ajaxify="false" class="thumbnail img-thumbnail">
-				<img src="http://designcreateplay.com/portfolio/work/nodebb5_thumb.png" alt="">
-			</a>
-			<a href="http://designcreateplay.com/portfolio/work/nodebb6.png" data-ajaxify="false" class="thumbnail img-thumbnail">
-				<img src="http://designcreateplay.com/portfolio/work/nodebb6_thumb.png" alt="">
-			</a>
-			<a href="http://designcreateplay.com/portfolio/work/nodebb7.png" data-ajaxify="false" class="thumbnail img-thumbnail">
-				<img src="http://designcreateplay.com/portfolio/work/nodebb7_thumb.png" alt="">
-			</a>
+			<!-- END gallery -->
 		</span>
 	</p>
 </div>
