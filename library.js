ecfad61c4a2f224c2536b8aed152584dc6bab001
psychobunny <psycho.bunny@hotmail.com>
2014-01-22 17:36:20 -0500

    refactored code; updated plugin to show an example of how to serve more than one static page (added /help)

--- library.js
+++ library.js
@@ -1,10 +1,9 @@
 var	fs = require('fs'),
 	path = require('path'),
+	async = require('async'),
 	templates = module.parent.require('../public/src/templates.js');
 	
 var constants = Object.freeze({
-	'route': '/about',
-	'name': "About NodeBB",
 	'gallery': [
 		{
 			image: "http://designcreateplay.com/portfolio/work/nodebb1.png",
@@ -39,38 +38,102 @@ var constants = Object.freeze({
 
 
 var StaticPage = {
-		addNavigation: function(custom_header, callback) {
-			custom_header.navigation.push({
+	routes: {},
+	templates: {}
+};
+
+StaticPage.addNavigation = function(custom_header, callback) {
+	custom_header.navigation = custom_header.navigation.concat(
+		[
+			{
 				"class": "",
-				"route": constants.route,
-				"text": constants.name
-			});
+				"route": '/about',
+				"text": "About NodeBB"
+			},
+			{
+				"class": "",
+				"route": '/help',
+				"text": "Help"
+			}
+		]
+	);
 
-			return custom_header;
-		},
-		addRoute: function(custom_routes, callback) {
-			fs.readFile(path.resolve(__dirname, './static/about.tpl'), function (err, template) {
-				var content = templates.prepare(template.toString()).parse({
-					gallery: constants.gallery
-				});
+	return custom_header;
+};
 
-				custom_routes.routes.push({
-					"route": constants.route,
-					"method": "get",
-					"options": function(req, res, callback) {
-						callback({
-							req: req,
-							res: res,
-							route: constants.route,
-							name: constants.name,
-							content: content
-						});
-					}
-				});
+StaticPage.addRoute = function(custom_routes, callback) {
+	var templatesToLoad = ["about.tpl", "help.tpl"];
 
-				callback(null, custom_routes);
-			});
+	function loadTemplate(template, next) {
+		fs.readFile(path.resolve(__dirname, './static/templates/' + template), function (err, data) {
+			StaticPage.templates[template] = data.toString();
+			next(err);
+		});
+	}
+
+	async.each(templatesToLoad, loadTemplate, function(err) {
+		if (err) {
+			throw new Error("Error loading templates: "  + err);
 		}
-	};
+
+		custom_routes.routes = custom_routes.routes.concat(
+			[
+				{
+					"route": '/about',
+					"method": "get",
+					"options": StaticPage.routes.about
+				}
+			],
+			[
+				{
+					"route": "/help",
+					"method": "get",
+					"options": StaticPage.routes.help
+				}
+			]
+		);
+
+		callback(err, custom_routes);
+	});
+
+	fs.readFile(path.resolve(__dirname, './static/about.tpl'), function (err, template) {
+		var content = templates.prepare(template.toString()).parse({
+			gallery: constants.gallery
+		});
+
+		custom_routes.routes.push({
+			"route": '/about',
+			"method": "get",
+			"options": function(req, res, callback) {
+				callback({
+					content: content
+				});
+			}
+		});
+
+		callback(null, custom_routes);
+	});
+};
+
+
+StaticPage.routes.about = function(req, res, callback) {
+	var content = templates.prepare(StaticPage.templates['about.tpl']).parse({
+		gallery: constants.gallery
+	});
+
+	callback({
+		content: content
+	});
+};
+
+StaticPage.routes.help = function(req, res, callback) {
+	var content = templates.prepare(StaticPage.templates['help.tpl']).parse({
+		help_content: "Here's some default help content"
+	});
+
+	callback ({
+		content: content
+	})
+};
 
 module.exports = StaticPage;
---
+++ static/templates/help.tpl
@@ -0,0 +1,5 @@
+<div class="help">
+	<h2>Help Page</h2>
+	<p>This is an example of a help page</p>
+	<p>{help_content}</p>
+</div>
